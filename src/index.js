3191b7c09bc1a789327d3c4ac24218a60121e280
David <davidwonchung@gmail.com>
2022-02-07 14:25:23 -0600

    Continuous integration setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('73d93d19b1de47418c6ff8ae24d4c59f').install();
 
 ReactDOM.render(
   <React.StrictMode>
