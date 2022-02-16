ff7d4f4bd29ad73fda2de2fe0867ba041962ffb2
David <davidwonchung@gmail.com>
2022-02-16 11:33:49 -0600

    Added chart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './nprogress.css'
 import { WarningAlert } from './Alert';
 import { Container, Row, Col } from 'react-bootstrap';
 import WelcomeScreen from './WelcomeScreen';
+import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
 
 class App extends Component {
   state = {
@@ -56,6 +57,17 @@ class App extends Component {
     });
     this.updateEvents(currentLocation, eventCount);
   }
+
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location) =>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
 
@@ -63,13 +75,35 @@ class App extends Component {
       <Container className='app-container' fluid>
       <div className='App'>
       {!navigator.onLine ? (<WarningAlert text="You are offline! No new updates!" />) : (<WarningAlert text=''  />)}
+      <h1>Meet App</h1>
         <Row>
           <Col>
-          <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+            <h4>Choose your nearest city</h4>
+            <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
           </Col>
           
           <Col>
           <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
+          </Col>
+
+          <Col>
+            <h4>Events in each city</h4>
+
+          <ResponsiveContainer height={400} >
+            <ScatterChart
+              width={400}
+              height={400}
+              margin={{
+                top: 20, right: 20, bottom: 20, left: 20,
+                }}
+              >
+                <CartesianGrid />
+                <XAxis type="category" dataKey="city" name="city" />
+                <YAxis type="number" dataKey="number" name="number of events" />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
           </Col>
 
           <Col>
--- src/api.js
+++ src/api.js
@@ -24,6 +24,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
+    // eslint-disable-next-line
     const url = 'https://3vos0i3r1d.execute-api.us-east-1.amazonaws.com/dev/api/get-events' + '/' + token;
     const result = await axios.get(url);
     if (result.data) {
@@ -83,6 +84,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
+    // eslint-disable-next-line
     'https://3vos0i3r1d.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode
   )
     .then((res) => {
